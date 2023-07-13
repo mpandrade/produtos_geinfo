3a8bdfced487caa4b668479bae2270eaa43bb77f
Deniz Faccin <denizfaccin@gmail.com>
2023-07-13 14:30:48 -0300

    novas queries

---
+++ 02. Solicitacoes de Dados/0098.23. Listagens Previne Metabase - Dep Info/query5.sql
@@ -0,0 +1,91 @@
+----Cobertura vacinal de Poliomielite inativada e de Pentavalente
+with
+	quad as (
+		select
+			inicio_quad,
+			fim_quad,
+			max_quad,
+			to_char(fim_quad,'YYYY')||
+				case
+					when to_char(fim_quad, 'MM') = '04' then '_Q1'
+					when to_char(fim_quad, 'MM') = '08' then '_Q2'
+					when to_char(fim_quad, 'MM') = '12' then '_Q3'
+				end as quad_texto
+			from (
+				select
+					coalesce((lag(inicio_quad) over()), fim_quad - interval '4 months')::date as inicio_quad,
+					(fim_quad - interval '1 day')::date as fim_quad,
+					(fim_quad + interval '8 months' - interval '1 day')::date as max_quad
+				FROM (
+					SELECT
+						generate_series(date_trunc('year', current_date - interval '1 year')::date, current_date + interval '1 year', interval '4 months') as inicio_quad,
+						generate_series(date_trunc('year', current_date - interval '1 year')::date, current_date + interval '1 year' + interval '4 months', interval '4 months') as fim_quad
+				) t1
+			) t2
+			where --filtra o quad atual na série
+				fim_quad >= current_date
+				and inicio_quad <= current_date
+		),
+		criancas_quad as (
+		select 
+			uc.cd_usu_cadsus,
+			uc.dt_nascimento,
+			coalesce(em.descricao, em2.descricao, 'Sem unidade') as unidade,
+			coalesce(eqa.cd_area::text, eqa2.cd_area::text, 'Sem equipe') as equipe,
+			count(uc.cd_usu_cadsus) as quant_criancas
+		from 
+			usuario_cadsus uc 
+			left join endereco_usuario_cadsus euc on uc.cd_endereco = euc.cd_endereco 
+			-- Joins para equipe de acompanhamento	
+			left join equipe eq on uc.cd_equipe = eq.cd_equipe
+			left join equipe_area eqa on eq.cd_equipe_area = eqa.cd_equipe_area
+			left join empresa em on eq.empresa = em.empresa 
+			-- Joins para equipe definida pelo endereço estruturado	
+			left join endereco_estruturado ee on euc.cd_endereco_estruturado = ee.cd_endereco_estruturado 
+			left join equipe_micro_area ema on ema.cd_eqp_micro_area = ee.cd_eqp_micro_area 
+			left join equipe_area eqa2 on ema.cd_equipe_area = eqa2.cd_equipe_area 
+			left join empresa em2 on ema.empresa = em2.empresa
+			join quad q on (uc.dt_nascimento + interval '1 year') between q.inicio_quad and q.fim_quad
+		where 
+			uc.st_excluido = 0
+			and uc.st_vivo = 1
+			and uc.situacao in (0,1)
+			and uc.flag_unificado = 0
+			and uc.cd_municipio_residencia = 420540
+		group by 1,2,3,4
+		),
+		vacinas as (
+		select
+			uc.cd_usu_cadsus,
+			max(case when tv.cd_vacina in ('3127216','81982','1550322') then 1 else 0 end) as vip,
+			max(case when tv.cd_vacina in ('1550326','81985','1550322') then 1 else 0 end) as penta
+		from
+			vac_aplicacao va
+			join tipo_vacina tv on tv.cd_vacina = va.cd_vacina
+			join usuario_cadsus uc on va.cd_usu_cadsus = uc.cd_usu_cadsus
+		where
+			va.status = 1
+			and va.cd_doses = 3 
+			and tv.cd_vacina in ('81982','3127216','1550326','81985','1550322') 
+		group by 1
+		)
+select 
+	q.inicio_quad,
+	q.fim_quad,
+	q.quad_texto,
+	cri.cd_usu_cadsus,
+	uc.nm_usuario,
+	uc.dt_nascimento,
+	concat_ws(' ',euc.nm_logradouro, euc.nr_logradouro, euc.nm_comp_logradouro) as endereco,
+	age(uc.dt_nascimento) as idade,
+	cri.unidade,
+	cri.equipe,
+	case when coalesce(vac.vip,0) = 1 then 'Sim' else 'Não' end as vip,
+	case when coalesce(vac.penta,0) = 1 then 'Sim' else 'Não' end as penta,
+	case when coalesce(vac.vip,0) = 1 and coalesce(vac.penta,0) = 1 then 'Ok!' else 'Vac.Incompleta' end as situacao
+from 
+	criancas_quad cri
+	left join vacinas vac on cri.cd_usu_cadsus = vac.cd_usu_cadsus
+	join quad q on true
+	join usuario_cadsus uc on cri.cd_usu_cadsus = uc.cd_usu_cadsus 
+	left join endereco_usuario_cadsus euc on uc.cd_endereco = euc.cd_endereco
--- 02. Solicitacoes de Dados/0101.23. Estudo Odonto CS Estreito - Dep Info.sql
+++ 02. Solicitacoes de Dados/0101.23. Estudo Odonto CS Estreito - Dep Info.sql
@@ -1,1 +1,72 @@
---SQL?
+
+select 
+	pro.nm_profissional,
+	to_char(atd.dt_atendimento, 'MM/YYYY') as mes_ano,
+	count(atd.nr_atendimento) as qt_atd
+from 
+	atendimento atd 
+	join usuario_cadsus uc on atd.cd_usu_cadsus = uc.cd_usu_cadsus 
+	join profissional pro on atd.cd_profissional = pro.cd_profissional
+where 
+	atd.cd_profissional in (423056023,263569)
+	and atd.dt_atendimento::date between '2021-07-01'::date and '2023-06-30'::date
+group by 1,2
+order by 1,2
+
+
+
+
+
+Endereços obtidos a partir de março de 2022 - Entrada do Tales
+
+
+with 
+	usuarios_ana as (
+		select distinct
+			atd.cd_usu_cadsus
+		from 
+			atendimento atd 
+		where 
+			atd.cd_profissional = 263569
+			and atd.dt_atendimento::date between '2022-03-01'::date and '2023-06-30'::date
+		),
+	usuarios_tales as (
+		select distinct
+			atd.cd_usu_cadsus
+		from 
+			atendimento atd 
+		where 
+			atd.cd_profissional = 423056023
+			and atd.dt_atendimento::date between '2022-03-01'::date and '2023-06-30'::date
+		)
+select 
+	'ANA PAULA MACANEIRO MELO' as profissional,
+	ana.cd_usu_cadsus,
+	tlc.ds_tipo_logradouro || ' ' || euc.nm_logradouro as logradouro,
+	euc.nr_logradouro,
+	euc.nm_bairro,
+	euc.cep,
+	eq.nm_referencia as equipe_acomp
+from 
+	usuario_cadsus uc 
+	join usuarios_ana ana on uc.cd_usu_cadsus = ana.cd_usu_cadsus
+	join endereco_usuario_cadsus euc on uc.cd_endereco = euc.cd_endereco 
+	join tipo_logradouro_cadsus tlc on euc.cd_tipo_logradouro = tlc.cd_tipo_logradouro
+	left join equipe eq on uc.cd_equipe = eq.cd_equipe
+
+union all 
+
+select 
+	'TALES SEVERIANO DA SILVA' as profissional,
+	tales.cd_usu_cadsus,
+	tlc.ds_tipo_logradouro || ' ' || euc.nm_logradouro as logradouro,
+	euc.nr_logradouro,
+	euc.nm_bairro,
+	euc.cep,
+	eq.nm_referencia as equipe_acomp
+from 
+	usuario_cadsus uc 
+	join usuarios_tales tales on uc.cd_usu_cadsus = tales.cd_usu_cadsus
+	join endereco_usuario_cadsus euc on uc.cd_endereco = euc.cd_endereco 
+	join tipo_logradouro_cadsus tlc on euc.cd_tipo_logradouro = tlc.cd_tipo_logradouro
+	left join equipe eq on uc.cd_equipe = eq.cd_equipe
